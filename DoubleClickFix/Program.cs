bbaf02e726ba234c7d0885d0b03726372df9973a
Christopher Nenning <cnenning@gmx.net>
2026-07-08 00:52:34 +0200

    use ApplicationConfiguration.Initialize and SystemAware DPI mode to avoid broken layout on runtime DPI changes

--- DoubleClickFix/Program.cs
+++ DoubleClickFix/Program.cs
@@ -62,8 +62,7 @@ internal class Program {
 
 	[STAThread]
 	private static void Main(string[] args) {
-		Application.EnableVisualStyles();
-		Application.SetCompatibleTextRenderingDefault(false);
+		ApplicationConfiguration.Initialize();
 
 		using Logger logger = new();
 
